dda2c29f994a2dffd755337c84c1b7b952684cf8
Adnaan-Alvi-au26 <alviadnan389@gmail.com>
2023-05-06 02:18:59 +0530

    RDBMS

--- dbConfig.js
+++ dbConfig.js
@@ -9,20 +9,3 @@ const pool = new Pool({
 });
 
 module.exports = pool
-
-
-// const mongoose = require('mongoose');
-// // const MongoUrl = database connection url have been removed before uploading 
-
-
-
-// async function connectDB(){
-//     try {
-//         await mongoose.connect(MongoUrl,{dbName:'NimapTask'})
-//         console.log('DB connection successfull')
-//     } catch (error) {
-//         console.log(error)
-//         process.exit()
-//     }
-// };
-// module.exports  = connectDB;
